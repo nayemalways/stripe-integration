7e8152e288fef763e9658f8ea1e15f8e3aaa01f8
nayemalways <nishanahmed13913@gmail.com>
2025-11-05 02:13:01 +0600

    Replaced backend url by https://stripe-server-vih5.onrender.com/

--- client/src/constant/index.jsx
+++ client/src/constant/index.jsx
@@ -1,5 +1,5 @@
 // export const backendURL = 'http://localhost:3000/api';
 // export const socketBackendURL = 'http://localhost:3000';
 
-export const socketBackendURL = 'https://stripe-server-pied.vercel.app';
-export const backendURL = 'https://stripe-server-pied.vercel.app/api';
+export const socketBackendURL = 'https://stripe-server-vih5.onrender.com';
+export const backendURL = 'https://stripe-server-vih5.onrender.com/api';
